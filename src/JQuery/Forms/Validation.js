91b0b5542c11f0a1d529fdf10e7b9b66a6021908
Samuvel-D <samuvel.d1999@gmail.com>
2022-11-14 11:51:55 +0530

    Changes

---
+++ src/JQuery/Forms/Validation.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    jQuery.validator.addMethod("Address",function(value,element){
+         if($("#Street").val()!=""&&$("#State").val()!=""&&$("#City").val()!=""&&$("#PinCode").val()!=""){
+            return true;
+        }
+        else if ($("#Street").val()==""&&$("#State").val()==""&&$("#City").val()==""&&$("#PinCode").val()==""){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    },"Please Fill All Address Fields")
+       $("#form").validate({
+           rules:{
+               FirstName:{required:true,minlength:3},
+               PinCode:{Address:true,digits:true,minlength:6,maxlength:6},
+               email:{required:true,email:true},
+               MobileNumber:{required:true,digits:true,minlength:10,maxlength:10}
+           },
+
+
+        })
+
+        $("#submit").click(function(){
+            event.preventDefault();
+            $("#form").valid();
+             var json=convertFormToJSON(form);
+            });
+        function convertFormToJSON(form) {
+            const array = $(form).serializeArray();
+            const json = {};
+            $.each(array, function () {
+            json[this.name] = this.value || "";
+            });
+            return json;
+            }
+
+
+})
